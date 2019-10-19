0b89ad7903fa218143cd169a192f1e8ba4df426f
Amit Cirt <siamitcs0@gmail.com>
2019-10-19 20:02:31 +0000

    Deploy samitc/vigoler-ui to github.com/samitc/vigoler-ui.git:gh-pages

---
+++ precache-manifest.c35bce7f009ea3c91f1dab8da51e5e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22180ba19bbfbf38bd59ce38e8e62aaf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "182f801f95aa7eada026",
+    "url": "./static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "ff66caf1ca2d471b507d",
+    "url": "./static/css/main.8463493c.chunk.css"
+  },
+  {
+    "revision": "182f801f95aa7eada026",
+    "url": "./static/js/2.5be8d42a.chunk.js"
+  },
+  {
+    "revision": "ff66caf1ca2d471b507d",
+    "url": "./static/js/main.d41ff06d.chunk.js"
+  },
+  {
+    "revision": "c87ec8cfb8846130ed66",
+    "url": "./static/js/runtime-main.536af0e0.js"
+  }
+]);
